e3ca531bff7f6998f6acfff7d5a7f0a063b5ed81
shivsan <shivku.goku@gmail.com>
2018-01-02 00:40:07 +0530

    First threads

--- ThreadDemo/Program.cs
+++ ThreadDemo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ThreadDemo
@@ -10,6 +11,21 @@ namespace ThreadDemo
     {
         static void Main(string[] args)
         {
+            // default thread
+            //Create threadstart delegate
+            var threadStart = new ThreadStart(WriteHello);
+            var thread = new Thread(threadStart);
+            thread.Start();
+
+            //Force main thread to wait for child thread
+            //thread.Join();
+
+            Console.WriteLine("End");
+         }
+
+        public static void WriteHello()
+        {
+            Console.WriteLine("Hello");
         }
     }
 }
